3d792d086e2112063fb93d720341d8bca84182ad
Van Anh <majanhqn@gmail.com>
2018-03-03 10:10:11 +0700

    Programmatic Constraints

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -20,7 +20,17 @@ class MapViewController : UIViewController {
         // create map view
         mapView = MKMapView()
         // set it into view
-        view = mapView 
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
         
     }
     
