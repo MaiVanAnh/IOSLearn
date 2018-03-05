a036d495e576f8921b33970507f1f8b277c03133
Van Anh <majanhqn@gmail.com>
2018-03-05 14:37:21 +0700

    Gold Challenge: Dropping Pins

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -18,6 +18,22 @@ class MapViewController : UIViewController, MKMapViewDelegate {
     
     var locationManager = CLLocationManager()
     
+    var index : Int!
+    
+    var coordinate1, coordinate2, coordinate3: CLLocationCoordinate2D!
+    
+    var string1 = "Quang Nam"
+    
+    var string1s = "The place where I was born"
+    
+    var string2 = "Da Nang"
+    
+    var string2s = "The city where I learn now"
+    
+    var string3 = "Tam Ki"
+    
+    var string3s = "The place Where i would like to visit"
+    
     override func loadView() {
         // create map view
         mapView = MKMapView()
@@ -25,6 +41,12 @@ class MapViewController : UIViewController, MKMapViewDelegate {
         view = mapView
         
         mapView.delegate = self
+        
+        coordinate1 = CLLocationCoordinate2DMake(15.634347, 108.249935)
+        
+        coordinate2 = CLLocationCoordinate2DMake(16.068896, 108.155556)
+        
+        coordinate3 = CLLocationCoordinate2DMake(15.567349, 108.477041)
         
         let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         
@@ -84,6 +106,44 @@ class MapViewController : UIViewController, MKMapViewDelegate {
         leadingConstraintBtn.isActive = true
         
         trailingConstraintBtn.isActive = true
+        
+        // gold challenge
+        
+        let buttonForGC = UIButton(type: UIButtonType.system)
+        
+        buttonForGC.backgroundColor = UIColor.yellow.withAlphaComponent(0.5)
+        
+        buttonForGC.setTitle("Pin Location", for: .normal)
+        
+        buttonForGC.setTitleColor(UIColor.blue ,for: .normal)
+        
+        buttonForGC.layer.cornerRadius = 5
+        
+        buttonForGC.addTarget(self, action: #selector(MapViewController.buttonActionForGoldChallenge(sender:)), for: .touchUpInside)
+        
+        buttonForGC.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(buttonForGC)
+        
+        let bottomConstraintBtnGC = buttonForGC.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
+        
+        let marginsBtnGC = view.layoutMarginsGuide
+        
+        let leadingConstraintBtnGC = buttonForGC.leadingAnchor.constraint(equalTo: marginsBtnGC.leadingAnchor)
+        
+        let trailingConstraintBtnGC = buttonForGC.trailingAnchor.constraint(equalTo: marginsBtnGC.trailingAnchor)
+        
+        bottomConstraintBtnGC.isActive = true
+        
+        leadingConstraintBtnGC.isActive = true
+        
+        trailingConstraintBtnGC.isActive = true
+        
+        
+        
+        
+        
+        
     }
     
     @objc func mapTypeChanged(_ segControl : UISegmentedControl) {
@@ -118,6 +178,67 @@ class MapViewController : UIViewController, MKMapViewDelegate {
         
         mapView.setRegion(region, animated: true)
         
+    }
+    
+    // gold challenge
+    
+    @objc func buttonActionForGoldChallenge(sender: UIButton!) {
+        switch index {
+            
+        case nil, 2:
+            index = 0
+            point(title: string1, subTitle: string1s, coordinate: coordinate1)
+            
+        case 0:
+            index = 1
+            point(title: string2, subTitle: string2s, coordinate: coordinate2)
+            
+        case 1:
+            index = 2
+            point(title: string3, subTitle: string3s, coordinate: coordinate3)
+            
+        default:
+            break
+        }
+    }
+    
+    private func point(title: String!, subTitle: String!, coordinate: CLLocationCoordinate2D) {
+        
+        let region = MKCoordinateRegionMakeWithDistance(coordinate, 700, 700)
+        
+        mapView.setRegion(region, animated: true)
+        
+        let point = MKPointAnnotation()
+        
+        point.coordinate = coordinate
+        
+        point.title = title
+        
+        point.subtitle = subTitle
+        
+        mapView.addAnnotation(point)
+  
+    }
+    
+    func mapViewForGoldChallenge(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+       
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "Text") as? MKPinAnnotationView
+        
+        if pinView == nil {
+            
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "Text")
+            
+            pinView!.canShowCallout = true
+            
+            pinView!.animatesDrop = true
+            
+            pinView!.pinTintColor = MKPinAnnotationView.purplePinColor()
+        } else {
+            
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
     override func viewDidLoad() {
         super.viewDidLoad()
