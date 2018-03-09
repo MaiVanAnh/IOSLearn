1c9636b575d2610bc28e727203a65153b32527db
Van Anh <majanhqn@gmail.com>
2018-03-09 22:28:32 +0700

    UITableView: Creating the Item class

---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Van Anh on 3/9/18.
+//  Copyright © 2018 Van Anh. All rights reserved.
+//
+
+//import Foundation
+
+import UIKit
+
+
+class ItemsViewController: UITableViewController {
+    
+}
