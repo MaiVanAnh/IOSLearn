2fbb866162c71c8e99c49a74fb2c7ac14c17783b
Van Anh <majanhqn@gmail.com>
2018-03-09 12:04:40 +0700

    can not debug same same the guide

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+    
+        for i in 0..<10 {
+            array.insert(i, at: 1)
+        }
+        
+        // go to step too far emptying the array (notice the range change)
+        
+        for _ in 0..<10 {
+            array.remove(0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
