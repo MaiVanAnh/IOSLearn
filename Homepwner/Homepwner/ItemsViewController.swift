c07d582e65cbccdcf82c0c17627014f83bc94273
Van Anh <majanhqn@gmail.com>
2018-04-05 00:00:36 +0700

    Using ItemCel

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -90,13 +90,19 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
         if indexPath.row < itemStore.allItems.count {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
             
             let item = itemStore.allItems[indexPath.row]
             
-            cell.textLabel?.text = item.name
+            //cell.textLabel?.text = item.name
             
-            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+           // cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            
+            cell.nameLabel.text = item.name
+            
+            cell.serialNumberLabel.text = item.serialNumber
+            
+            cell.valueLabel.text = "$\(item.valueInDollars)"
             
             return cell
         } else {
