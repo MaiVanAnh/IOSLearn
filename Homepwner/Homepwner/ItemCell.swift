88d99e6ae3b43cee2b08112ca0a3ff8b3842c0e5
Van Anh <majanhqn@gmail.com>
2018-04-04 23:41:35 +0700

    Chapter 12: Subclassing UITableViewCell : Creating ItemCell

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Van Anh on 4/4/18.
+//  Copyright © 2018 Van Anh. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
